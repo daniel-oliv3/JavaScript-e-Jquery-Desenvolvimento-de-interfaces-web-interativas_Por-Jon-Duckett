15508787edbb47ef6c03be14055a29ad38c785d6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-14 16:04:30 -0300

    Usando um variável para armazenar uma string

---
+++ 02 - Instruções básicas de JavaScript/c02/js/string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var menssage;
+username = 'Molly';
+menssage = 'Veja a nossa próxima linha';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = menssage;
