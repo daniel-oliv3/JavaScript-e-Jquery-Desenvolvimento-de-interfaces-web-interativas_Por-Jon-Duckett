db1293a60f7caadc63277569e2a1e88e395d255c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-14 17:06:59 -0300

    Usando aspas dentro de uma string

---
+++ 02 - Instruções básicas de JavaScript/c02/js/string-with-quotes.js
@@ -0,0 +1,9 @@
+var title;
+var message;
+title = "Ofertas especiais da Molly";
+message = '<a href=\"sale.html\">25% off!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+var elNote = document.getElementById('note');
+elNote.innerHTML = message;
