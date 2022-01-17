186c784f4e9f6e717699bbaeea3ab6c54a88e982
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-17 11:13:54 -0300

    Construtor de Array

---
+++ 02 - Instruções básicas de JavaScript/c02/js/array-constructor.js
@@ -0,0 +1,11 @@
+var colors = new Array('white', 
+                       'black', 
+                       'custom');
+
+var el = document.getElementById('colors');
+el.innerHTML = colors.item(0);
+
+/*
+O array literal (mostrado no primeiro exeplo de codigo) e
+preferido em relação ao construtor de arrys ao criar arrays.
+*/
--- 02 - Instruções básicas de JavaScript/c02/js/array-literal.js
+++ 02 - Instruções básicas de JavaScript/c02/js/array-literal.js
@@ -3,4 +3,4 @@ var colors;
 colors = ['white', 'black', 'custom'];
 
 var el = document.getElementById('colors');
-el.textContent = colors[2];
+el.textContent = colors[0];
