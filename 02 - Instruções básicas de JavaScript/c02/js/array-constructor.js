34de44ba32a3ab0e950ee26be38f487efae8961f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-17 11:33:25 -0300

    Update

--- 02 - Instruções básicas de JavaScript/c02/js/array-constructor.js
+++ 02 - Instruções básicas de JavaScript/c02/js/array-constructor.js
@@ -3,9 +3,17 @@ var colors = new Array('white',
                        'custom');
 
 var el = document.getElementById('colors');
-el.innerHTML = colors.item(0);
+el.innerHTML = colors[0];
 
 /*
 O array literal (mostrado no primeiro exeplo de codigo) e
 preferido em relação ao construtor de arrys ao criar arrays.
+*/
+
+/*
+NOTA: textContent não funciona no IE8 ou anterior
+Você pode usar innerHTML na linha 10, mas observe os problemas de segurança em p228-231
+el.innerHTML = cores[0];
+
+Houve um erro de digitação na primeira impressão na última linha deste exemplo, ele foi corrigido neste arquivo.
 */
